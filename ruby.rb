968f41bc6c47384277a32b5561041ac79f15f946
kayemba <kayembejonathan5@gmail.com>
2023-06-21 14:34:17 +0200

    Add method

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,16 @@
-puts "Hello, World!";
+class Person
+    def initialize(id,name,age)
+        @id = id
+        @name = name
+        @age = age
+    end
+
+    def intro
+    print "my name is #{@name} and my age is #{@age} " 
+    end
+end
+
+p1 = Person.new(1,"joe",24)
+p2 = Person.new(2,"james",34)
+p1.intro
+p2.intro
